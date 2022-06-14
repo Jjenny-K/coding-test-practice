43a842684aa00ee8bec8daa226fdd75c146d17fc
Jjenny-K <kkjjh0129@gmail.com>
2022-06-14 18:14:01 +0900

    Add: 선형 구조 코딩테스트 연습

---
+++ programmers/p_lv_01/python/같은 숫자는 싫어.py
@@ -0,0 +1,26 @@
+# 0 - 9로 이루어진 배열에서 연속적으로 나타나는 숫자를 하나만 남기고 제거한 배열 출력
+
+def solution_001(arr):
+    answer = [arr[0]]
+
+    for val in arr[1:]:
+        if answer[-1] != val:
+            answer.append(val)
+
+    print(answer)
+
+
+def solution_002(arr):
+    answer = []
+
+    for val in arr:
+        if answer[-1:] != [val]:
+            answer.append(val)
+
+    print(answer)
+
+
+# run result
+arr_001 = [1, 1, 3, 3, 0, 1, 1]
+solution_001(arr_001)
+solution_002(arr_001)
---
+++ programmers/p_lv_01/python/나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,29 @@
+# 배열 각 원소 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열 출력
+# 출력할 배열이 빈 배열인 경우 -1 채워 출력
+
+def solution_001(arr, divisor):
+    answer = [i for i in arr if i % divisor == 0]
+    answer.sort()
+
+    if len(answer) == 0:
+        answer.append(-1)
+
+    print(answer)
+
+
+def solution_002(arr, divisor):
+    answer = sorted([i for i in arr if i % divisor == 0])
+
+    print(answer if len(answer) != 0 else [-1])
+
+
+def solution_003(arr, divisor):
+    print(sorted([i for i in arr if i % divisor == 0]) or [-1])
+
+
+# run result
+arr_001 = [5, 9, 7, 10]
+divisor_001 = 5
+solution_001(arr_001, divisor_001)
+solution_002(arr_001, divisor_001)
+solution_003(arr_001, divisor_001)
