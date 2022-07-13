dfa73aae95a798a3faaa7b22790e9422a6cd7c01
Jjenny-K <kkjjh0129@gmail.com>
2022-07-13 17:47:36 +0900

    Add: 비선형 자료구조 코딩테스트 연습

---
+++ programmers/p_lv_01/python/숫자 문자열과 영단어.py
@@ -0,0 +1,30 @@
+# 문자열 변환
+
+# 일부 자릿수를 영단어로 바꾼 숫자에서 원래 숫자 출력
+# 각 숫자는 영어 영단어와 대응
+
+
+def solution_001(s):
+    words = {'0': 'zero', '1': 'one', '2': 'two', '3': 'three', '4': 'four',
+             '5': 'five', '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine'}
+
+    for k, v in words.items():
+        s = s.replace(v, k)
+
+    print(int(s))
+
+
+def solution_002(s):
+    words = ['zero', 'one', 'two', 'three', 'four',
+             'five', 'six', 'seven', 'eight', 'nine']
+
+    for i, v in enumerate(words):
+        s = s.replace(v, str(i))
+
+    print(int(s))
+
+
+# run result
+s_001 = '2three45sixseven'
+solution_001(s_001)
+solution_002(s_001)
---
+++ programmers/p_lv_01/python/신규 아이디 추천.py
@@ -0,0 +1,46 @@
+# 정규표현식 # 문자열 변환
+
+# 신규 유저가 입력한 아이디 new_id가 주어질 때 아이디 규칙에 맞는 추천 아이디 출력
+"""
+    # 아이디 규칙
+    1. new_id 모든 대문자를 소문자로 치환
+    2. 알파벳 소문자, 숫자, '-', '_', '.'를 제외한 모든 문자 제거
+    3. '.'가 2번 이상 연속될 경우 '.'(하나의 마침표)로 치환
+    4. '.'가 처음이나 끝에 위치한다면 제거
+    5. new_id가 비었을 경우 'a' 추가
+    6. new_id 길이가 16자 이상일 경우 첫 15개 문자 제외한 모든 문자 제거(제거 후 '.'가 끝에 위치한다면 제거)
+    7. new_id 길이가 2자 이하일 경우 new_id 길이가 3이 될 때까지 마지막 문자 반복 추가
+"""
+import re
+
+
+def solution_001(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub(r'[^a-z0-9-_.]', '', new_id)
+    new_id = re.sub(r'[.]{2,}', '.', new_id)
+    new_id = new_id[1:] if new_id.startswith('.') else new_id
+    new_id = 'a' if len(new_id) == 0 else new_id
+    new_id = new_id[:15] if len(new_id) >= 16 else new_id
+    # 상위에서 '.'(하나의 마침표)로 치환되기 때문에 일정 조건 이후 최종적으로 마지막 '.' 제거
+    new_id = new_id[:-1] if new_id.endswith('.') else new_id
+    new_id = new_id + ''.join(new_id[-1] for _ in range(3 - len(new_id))) if len(new_id) <= 2 else new_id
+
+    print(new_id)
+
+
+def solution_002(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id)
+    new_id = re.sub('\.+', '.', new_id)
+    new_id = re.sub('^[.]|[.]$', '', new_id)
+    new_id = 'a' if len(new_id) == 0 else new_id[:15]
+    new_id = re.sub('^[.]|[.]$', '', new_id)
+    new_id = new_id if len(new_id) > 2 else new_id + "".join([new_id[-1] for _ in range(3 - len(new_id))])
+
+    print(new_id)
+
+
+# run result
+new_id_001 = '...!@BaT#*..y.abcdefghijklm'
+solution_001(new_id_001)
+solution_002(new_id_001)
