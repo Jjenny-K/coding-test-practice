7456746eaab2d346df088a1161e296c84bb0a9ae
Jjenny-K <kkjjh0129@gmail.com>
2022-07-14 19:15:03 +0900

    Add: 탐색 코딩테스트 연습

---
+++ programmers/p_lv_01/python/모의고사.py
@@ -0,0 +1,53 @@
+# 수포자 3명, 정답이 순서대로 들은 배열 answers에서 가장 많은 문제를 맞힌 사람을 배열로 출력
+# 가장 많은 문제를 맞힌 사람이 여럿일 경우 오름차순 정렬
+"""
+    수포자의 찍는 방식
+    1번 수포자: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+    2번 수포자: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+    3번 수포자: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+"""
+
+
+def solution_001(answers):
+    answer = {x: 0 for x in range(1, 4)}
+
+    stu_a = [1, 2, 3, 4, 5]
+    stu_b = [2, 1, 2, 3, 2, 4, 2, 5]
+    stu_c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    stu_a = stu_a * (10000 // len(stu_a))
+    stu_b = stu_b * (10000 // len(stu_b))
+    stu_c = stu_c * (10000 // len(stu_c))
+
+    for idx, value in enumerate(answers):
+        if value == stu_a[idx]:
+            answer[1] += 1
+        if value == stu_b[idx]:
+            answer[2] += 1
+        if value == stu_c[idx]:
+            answer[3] += 1
+
+    print([key for key, value in answer.items() if value == max(answer.values())])
+
+
+def solution_002(answers):
+    stu_a = [1, 2, 3, 4, 5]
+    stu_b = [2, 1, 2, 3, 2, 4, 2, 5]
+    stu_c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
+
+    for idx, answer in enumerate(answers):
+        if answer == stu_a[idx % len(stu_a)]:
+            score[0] += 1
+        if answer == stu_b[idx % len(stu_b)]:
+            score[1] += 1
+        if answer == stu_c[idx % len(stu_c)]:
+            score[2] += 1
+
+    print([idx + 1 for idx, value in enumerate(score) if value == max(score)])
+
+
+# run result
+answers_001 = [1, 3, 2, 4, 2]
+solution_001(answers_001)
+solution_002(answers_001)
---
+++ programmers/p_lv_01/python/키패드 누르기.py
@@ -0,0 +1,48 @@
+# 맨해튼 거리 # DFS, BFS...?
+
+# 전화 키패드에서 왼손과 오른손의 엄지손가락만 이용해서 숫자를 입력할 때 사용되는 손 위치 출력
+# 초기 왼손 엄지손가락 '*', 오른손 엄지손가락 '#'
+# 상하좌우 4가지 방향으로만 이동, 키패드 이동 한 칸 == 거리 1 이동
+# 1, 4, 7 입력시 왼손 엄지손가락, 3, 6, 9 입력시 오른손 엄지손가락 사용
+# 2, 5, 8, 0 입력시 두 엄지손가락 현재 위치에서 더 가까운 엄지손가락 사용, 같을 경우 hand 사용
+
+
+def solution(numbers, hand):
+    answer = ''
+    keypad = {1: [0, 0], 2: [0, 1], 3: [0, 2],
+              4: [1, 0], 5: [1, 1], 6: [1, 2],
+              7: [2, 0], 8: [2, 1], 9: [2, 2],
+              '*': [3, 0], 0: [3, 1], '#': [3, 2]}
+    left, right = '*', '#'
+
+    for number in numbers:
+        if number in [1, 4, 7, '*']:
+            answer += 'L'
+        elif number in [3, 6, 9, '#']:
+            answer += 'R'
+        else:
+            nx, ny = keypad[number]
+            lx, ly = keypad[left]
+            rx, ry = keypad[right]
+
+            abs_left = (abs(nx - lx) + abs(ny - ly))
+            abs_right = (abs(nx - rx) + abs(ny - ry))
+            if abs_left > abs_right:
+                answer += 'R'
+            elif abs_left < abs_right:
+                answer += 'L'
+            else:
+                answer += hand.upper()[0]
+
+        if answer[-1] == 'L':
+            left = number
+        else:
+            right = number
+
+    print(answer)
+
+
+# run result
+numbers_001 = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
+hand_001 = 'left'
+solution(numbers_001, hand_001)
