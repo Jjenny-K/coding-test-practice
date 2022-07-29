998e8a6874f97d2ab40b6fa2abe2a2e463d4b209
Jjenny-K <kkjjh0129@gmail.com>
2022-07-29 17:13:07 +0900

    Add: 자료형 코딩테스트 연습

---
+++ programmers/p_lv_02/python/최솟값 만들기.py
@@ -0,0 +1,18 @@
+# 연산 # min # 정렬
+
+# 길이가 같은 두 배열의 수를 각각 하나씩 뽑아 곱해서 누적해 더했을 때 최솟값 출력
+
+def solution_001(A, B):
+    # return sum([i * j for i, j in zip(sorted(A), reversed(sorted(B)))])
+    return sum([i * j for i, j in zip(sorted(A), sorted(B, reverse=True))])
+
+
+def solution_002(A, B):
+    return sum(A.pop(A.index(min(A))) * B.pop(B.index(max(B))) for i in range(len(A)))
+
+
+# run result
+A_001 = [1, 4, 2]  # answer = 29
+B_001 = [5, 4, 4]
+print(solution_001(A_001, B_001))
+print(solution_002(A_001, B_001))
