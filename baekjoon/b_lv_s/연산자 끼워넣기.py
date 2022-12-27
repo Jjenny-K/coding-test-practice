132f7ec13401315f64febf3e1e683f9d93bcbd40
Jjenny-K <kkjjh0129@gmail.com>
2022-12-27 19:02:36 +0900

    Fix: 코딩테스트 풀이 추가

--- baekjoon/b_lv_s/연산자 끼워넣기.py
+++ baekjoon/b_lv_s/연산자 끼워넣기.py
@@ -4,6 +4,7 @@
 # 음수를 양수로 나눌 때는 수를 양수로 바꾸어 몫을 취하고 그 몫을 음수로 바꿈
 # 수와 수 사이에 연산자를 하나씩 끼워 넣어 수식을 만들었을 때, 결과가 최대인 것과 최소인 것 출력
 
+""" DFS 활용 """
 N = int(input())
 numbers = list(map(int, input().split()))
 operators = list(map(int, input().split()))  # +, -, *, // 순서
@@ -34,4 +35,40 @@ def calculation(cnt, total, plus, minus, multiply, divide):
 
 
 calculation(1, numbers[0], operators[0], operators[1], operators[2], operators[3])
+print(maximum, minimum, sep='\n')
+
+
+
+""" library - itertools 활용 """
+import itertools
+
+N = int(input())
+numbers = list(map(int, input().split()))
+operators = list(map(int, input().split()))  # +, -, *, // 순서
+
+ops = '+ ' * operators[0] + '- ' * operators[1] + '* ' * operators[2] + '// ' * operators[3]
+ops_list = ops.split()
+ops_perm = set(itertools.permutations(ops_list, len(ops_list)))
+
+maximum, minimum = -1e9, 1e9
+
+for value in ops_perm:
+    total = numbers[0]
+
+    for idx, op in enumerate(value):
+        if op == '+':
+            total += numbers[idx + 1]
+        elif op == '-':
+            total -= numbers[idx + 1]
+        elif op == '*':
+            total *= numbers[idx + 1]
+        elif op == '//':
+            if total < 0:
+                total = -(abs(total) // numbers[idx + 1])
+            else:
+                total //= numbers[idx + 1]
+
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+
 print(maximum, minimum, sep='\n')
